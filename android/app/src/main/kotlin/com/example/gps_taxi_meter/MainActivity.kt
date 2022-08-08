315f1f475224cc7eb352d03f9c6cebefa4cc00a6
supun2010 <salitha.supun.w@gmail.com>
2022-08-08 18:27:15 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/gps_taxi_meter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gps_taxi_meter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:gps_taxi_meter/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeView(),
+    );
+  }
+}
